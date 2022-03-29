01ba77f53aca7625bed8a7294ac572bed064e136
AndreySitnikov34 <ac1970@yandex.ru>
2022-03-29 12:53:42 +0300

    29-03

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@ export class Api {
   constructor(userCheck) {
     this._url = userCheck["url"];
     this._headers = userCheck["headers"];
-    this.parseResponse = this.parseResponse.bind(this);
+    // this.parseResponse = this.parseResponse.bind(this);
   }
   // config = {
   //   url: "https://nomoreparties.co/v1/plus-cohort7",
@@ -12,26 +12,18 @@ export class Api {
   //   },
   // };
   //Парсинг ответа
-  parseResponse(res) {
+  _parseResponse(res) {
     console.log("приходит ответ 16", res);
-    // if (res.ok) {
-    //   return res.json();
-    // }
-    // return Promise.reject(`Ошибка: ${res.status}`);
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
   }
   //Вытягиваие карточек с сервера
   getCards() {
     return fetch(`${this._url}/cards`, {
       headers: this._headers,
-    }).then((res) => {
-      // console.log("Api getCards - str 27", res);
-      // parseResponse(res);
-      if (res.ok) {
-        console.log("Api Ответ от сервевра - str 30", res);
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
-    });
+    }).then((res) => this._parseResponse(res));
   }
   //Добавление своей карточки
   postCard(card) {
@@ -78,12 +70,12 @@ export class Api {
       headers: this._headers,
     }).then((res) => {
       console.log("Api - str 74 - GET BY USER", res);
-      // parseResponse(res);
-      if (res.ok) {
-        // console.log("Ответ от сервевра 70", res);
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
+      parseResponse(res);
+      // if (res.ok) {
+      //   // console.log("Ответ от сервевра 70", res);
+      //   return res.json();
+      // }
+      // return Promise.reject(`Ошибка: ${res.status}`);
     });
   }
   //Добавление пользователя
